2aead950750aeaa6160ab5654a820ac62f570a52
hectorelmas <124789223+hmorhid1407@users.noreply.github.com>
2023-03-28 10:53:47 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
  */
 basic.forever(function () {
     for (let index = 0; index <= 4; index++) {
-        for (let index2 = 0; index2 <= 4; index2++) {
+        for (let index2 = 0; index2 <= index2; index2++) {
             led.plot(index, index2)
         }
         basic.pause(9)
