b051f7b0b4f9d3acdbbd6829b73f1514f419a4a5
hectorelmas <124789223+hmorhid1407@users.noreply.github.com>
2023-03-28 10:53:05 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 /**
- * ykyrkduysururusrutt
+ * este es ambush de doors del juego de roblox
  */
 basic.forever(function () {
-    basic.clearScreen()
     for (let index = 0; index <= 4; index++) {
         for (let index2 = 0; index2 <= 4; index2++) {
             led.plot(index, index2)
