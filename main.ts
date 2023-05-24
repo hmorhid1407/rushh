45e7b9a4f8d4e45eb6aa85a31d0a5cd90979e1d7
hectorelmas <124789223+hmorhid1407@users.noreply.github.com>
2023-05-24 13:20:04 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,17 @@
 /**
- * este es ambush de doors del juego de roblox
+ * este es A-60 de doors del juego de roblox
  */
 basic.forever(function () {
     for (let index = 0; index <= 4; index++) {
         for (let index2 = 0; index2 <= 4; index2++) {
             led.plot(index, index2)
         }
-        basic.pause(9)
+        basic.pause(1)
     }
     for (let index = 0; index <= 4; index++) {
         for (let index2 = 0; index2 <= 4; index2++) {
             led.unplot(index, index2)
         }
-        basic.pause(9)
+        basic.pause(1)
     }
 })
